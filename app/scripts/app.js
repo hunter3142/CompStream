5aa21ecd540545a2793041d9838ded9bb00930c6
Mathew <hunter3142@gmail.com>
2017-10-13 00:54:29 -0400

    First draft of a filter function

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,12 +1,36 @@
 angular.module('compStream', [])
 	.controller('StreamListController', function() {
 		var streamList = this;
-		var hulu = {provider: "Hulu", live: true};
-		var netflix = {provider: "Netflix", live: false};
-		var amazonPrime = {provider: "Amazon Prime", live: false};
-		var youtubeTV = {provider: "Youtube TV", live: true};
-		var hdAntenna = {provider: "HD Antenna", live: true};
 
-		streamList.providers = [hulu, netflix, amazonPrime, youtubeTV, hdAntenna];
-		streamList.liveTV = [hulu, youtubeTV, hdAntenna];
+		streamList.providers = [
+			{provider: "Hulu", live: true},
+			{provider: "Netflix", live: false},
+			{provider: "Amazon Prime", live: false},
+			{provider: "Youtube TV", live: true},
+			{provider: "HD Antenna", live: true}];
+
+		streamList.filteredResults = streamList.providers;
+
+		streamList.updateList = function() {
+			var options = {
+				live: streamList.liveChecked
+			}
+			streamList.filteredResults = streamList.filterBy(options);
+		}
+
+		streamList.filterBy = function(options) {
+			var filteredArray = [];
+			streamList.providers.filter(function(p) {
+				var match = false;
+				for(opt in options) {
+					if(!options[opt]) {
+						filteredArray = streamList.providers;
+					} else if(!match && p[opt] == options[opt]) {match = true;}
+				}
+				if(match) {
+					filteredArray.push(p)
+				}
+			})
+			return filteredArray
+		}
 	});
